75a6c196aff43abc43a649d7e54e1895d8dc904a
aidan <aidanguarniere@gmail.com>
2021-02-23 14:25:32 -0800

    Comment routes and population to Pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,15 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      // populate comments from Comment
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      // show newest pizza first
+      .select("-__v")
+      // sort in descending order by the _id value of Pizza
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
