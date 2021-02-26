aadf4615fae96afd94eece56717a8933bdaaf4dc
aidan <aidanguarniere@gmail.com>
2021-02-25 16:19:48 -0800

    Reply controllers and routes

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,61 @@
 // requirements
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types, } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String,
+// Reply schema
+const ReplySchema = new Schema(
+  {
+    // custom id to avoid confusion with parent comment_id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String,
+    },
+    writtenBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  commentBody: {
-    type: String,
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Comment schema
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    replies: [ReplySchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// create virtual to get total number of replies per Comment
+CommentSchema.virtual("replyCount").get(function () {
+  return this.replies.length;
 });
 
 const Comment = model("Comment", CommentSchema);
